640d5dbc9392c7dfb62f1e8ed889e0a6d7aeb275
Sumit Surin <surinsumit2@gmail.com>
2025-11-18 19:30:40 +0530

    Fix API base env variable

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-console.log("VITE_API_BASE =", import.meta.env.VITE_API_BASE);
-
 const App = () => {
   return (
     <Router>
